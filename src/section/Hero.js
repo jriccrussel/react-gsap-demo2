12e9838cc8535e1cc027ee97570963db081a700f
jriccrussel <jriccrussel@gmail.com>
2021-10-29 00:23:20 +0800

    complete hero section & animations

--- src/section/Hero.js
+++ src/section/Hero.js
@@ -1,17 +1,58 @@
-import React from 'react'
-import {TimelineLite ,TweenMax, Power3, gsap} from 'gsap';
+import React, { useEffect, useRef, useState } from 'react'
+import {TimelineLite ,TweenMax, Power3, gsap} from 'gsap'
 
-import imgGirl from "../assets/img/girl.webp";
-import imgBoy from "../assets/img/boy.webp";
+import imgGirl from "../assets/img/girl.webp"
+import imgBoy from "../assets/img/boy.webp"
 import arrow from '../assets/img/arrow-right-white.svg'
 
 const Hero = () => {
+    const hero = useRef(null)
+    const img = useRef(null) 
+    const content = useRef(null)
+
+    const tl = new TimelineLite({delay: 0.8})
+
+    useEffect(() => {
+        // console.log(hero.current)
+
+        // Images Vars
+        const girlImage = img.current.firstElementChild; // or children[0]
+        const boyImage = img.current.lastElementChild;
+
+        // Content Vars
+        const headlineFirst = content.current.children[0].children[0];
+        const headlineSecond = headlineFirst.nextSibling;
+        const headlineThird = headlineSecond.nextSibling;
+        const contentP = content.current.children[1];
+        const contentButton = content.current.children[2];
+
+        //Remove initial flash
+        // TweenMax.to(hero.current, 0, {css: {visibility: 'visible'}})
+        gsap.to(hero.current, 0, {css: {visibility: 'visible'}})
+
+        //Images Animation
+        tl.from(girlImage, 1.2, {y: 1280, ease: Power3.easeOut}, 'start')
+        .from(girlImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+        .from(boyImage, 1.4, {y: 1280, ease: Power3.easeOut}, .2)
+        .from(boyImage.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+
+        //Content Animation
+        tl.staggerFrom([headlineFirst.children, headlineSecond.children, headlineThird.children ], 1, {
+            y: 44,
+            ease:Power3.easeOut,
+            delay: .8
+        }, .15, 'start')
+        .from(contentP, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.4)
+        .from(contentButton, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1.6)
+
+    }, [tl])
+
     return (
-        <div className="hero">
+        <div className="hero" ref={hero}>
             <div className="container">
                 <div className="hero-inner">
                     <div className="hero-content">
-                        <div className="hero-content-inner">
+                        <div className="hero-content-inner" ref={content}>
                             <h1>
                                 <div className="hero-content-line">
                                     <div className="hero-content-line-inner">Relieving the burden</div>
@@ -37,7 +78,7 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className="hero-images">
-                        <div className="hero-images-inner">
+                        <div className="hero-images-inner" ref={img}>
                             <div className="hero-image girl">
                                 <img src={imgGirl} alt="" />
                             </div>
